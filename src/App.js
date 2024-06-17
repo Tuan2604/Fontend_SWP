4250939bdeb26f43fa4786babe52bc3c04093e3c
gialinh3009 <lintgse171158@fpt.edu.vn>
2024-06-17 23:37:10 +0700

    add UI post , create post , post management

--- src/App.js
+++ src/App.js
@@ -1,59 +1,100 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './Components/view/partials/Home';
-// import Login from './Components/view/Login/Login';
-// import Header from './Components/view/partials/Header'; // Import Header component
-// import Register from './Components/view/Register/Register';
-// import SignIn from './signln';
-
-
-// const App = () => {
-//   return (
-//     <Router>
-      
-//       <div>
-        
-//         <Header /> 
-       
-//         <Routes>
-          
-//           <Route path="/" element={<Home />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/Register" element={<Register />}/>
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-// src/App.js
-// src/App.js
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import Home from './Components/view/partials/Home';
-import Login from './Components/view/Login/Login';
-import Header from './Components/view/partials/Header'; 
-import Register from './Components/view/Register/Register';
-import './transitions.css'; // Import the transitions CSS
+import React, { Suspense, lazy, useState } from "react";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import Home from "./Components/view/partials/HomePage/Home";
+import Login from "./Components/view/Login/Login";
+import Header from "./Components/view/partials//HomePage/Header";
+import Register from "./Components/view/Register/Register";
+import "./transitions.css"; // Import the transitions CSS
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PostManagementPage from "./features/Saler/page/post-management";
+import PostCreate from "./features/Saler/page/post-create";
 
 const App = () => {
   const location = useLocation();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to handle login
+  const handleLogin = () => {
+    // Perform login logic here...
+    setIsLoggedIn(true);
+  };
+
+  // Function to handle logout
+  const handleLogout = () => {
+    // Perform logout logic here...
+    setIsLoggedIn(false);
+  };
+
+  const SalerRouter = lazy(() => import("./features/Saler"));
+
   return (
     <div>
-      <Header />
+      <ToastContainer position="top-right" autoClose={3000} />
       <TransitionGroup>
-        <CSSTransition
-          key={location.key}
-          classNames="fade"
-          timeout={300}
-        >
-          <Routes location={location}>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
+        <CSSTransition key={location.key} classNames="fade" timeout={300}>
+          <Suspense fallback={<>Loading</>}>
+            <Routes location={location}>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+                    <Home />
+                  </>
+                }
+              />
+              {/* Pass onLogin function to the Login component */}
+              <Route
+                path="/login"
+                element={
+                  <>
+                    <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+                    <Login onLogin={handleLogin} />
+                  </>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <>
+                    <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+                    <Register />
+                  </>
+                }
+              />
+              {/* <Route
+                path="/saler/*"
+                element={
+                  <>
+                    <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+                    <SalerRouter />
+                  </>
+                }
+              /> */}
+              <Route
+                path={"/saler/post-management"}
+                element={
+                  <>
+                    <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+                    <PostManagementPage />
+                  </>
+                }
+              />
+              <Route
+                path={"/saler/post-create"}
+                element={
+                  <>
+                    <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+                    <PostCreate />
+                  </>
+                }
+              />
+              <Route path="*" element={<Navigate to="/" replace />} />{" "}
+              {/* Catch-all route */}
+            </Routes>
+          </Suspense>
         </CSSTransition>
       </TransitionGroup>
     </div>
---
+++ src/Components/view/partials/HomePage/Header.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSignInAlt,
+  faUserPlus,
+  faSignOutAlt,
+  faBook,
+  faPen,
+  faBox,
+  faTools,
+  faSearch,
+  faBars,
+  faFile,
+  faListCheck,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Header.css";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const isLoggedIn = !!localStorage.getItem("email");
+
+  const logout = (event) => {
+    event.preventDefault();
+    if (window.confirm("Are you sure you want to logout?")) {
+      localStorage.clear();
+      navigate("/login");
+    }
+  };
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/search?query=${searchQuery}`);
+  };
+
+  return (
+    <header className="header">
+      <nav className="nav-container">
+        <div className="logo">
+          <Link to="/">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/FPT_logo_2010.svg/640px-FPT_logo_2010.svg.png"
+              alt="Home"
+              className="home-icon"
+            />
+          </Link>
+        </div>
+        <div className="search-container">
+          <form
+            onSubmit={handleSearchSubmit}
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              className="search-input"
+              placeholder="Search..."
+            />
+            <button type="submit" className="search-button">
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </form>
+        </div>
+        <div className="menu">
+          <ul style={{ minWidth: "600px" }}>
+            {!isLoggedIn && (
+              <>
+                <li>
+                  <Link to="/saler/post-management">
+                    <FontAwesomeIcon icon={faListCheck} /> Post Management
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/saler/post-create">
+                    <FontAwesomeIcon icon={faFile} /> Create Post
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/login">
+                    <FontAwesomeIcon icon={faSignInAlt} /> Login
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/register">
+                    <FontAwesomeIcon icon={faUserPlus} /> Register
+                  </Link>
+                </li>
+              </>
+            )}
+            {isLoggedIn && (
+              <>
+                <li className="dropdown">
+                  <a href="#danh-muc" onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={faBars} /> Danh mục
+                  </a>
+                  {showMenu && (
+                    <ul className="dropdown-menu">
+                      <li>
+                        <Link to="/tai-lieu">
+                          <FontAwesomeIcon icon={faBook} /> Tài liệu
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/dung-cu-hoc-tap">
+                          <FontAwesomeIcon icon={faPen} /> Dụng cụ học tập
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/vat-pham">
+                          <FontAwesomeIcon icon={faBox} /> Vật phẩm
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/thiet-bi-hoc-tap">
+                          <FontAwesomeIcon icon={faTools} /> Thiết bị học tập
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+                <li>
+                  <a href="#logout" onClick={logout}>
+                    <FontAwesomeIcon icon={faSignOutAlt} /> Logout
+                  </a>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
